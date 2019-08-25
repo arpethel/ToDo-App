8ea666381a7e1112db9e374d01cc259a1b570c33
arpethel <arpethel@gmail.com>
2019-08-25 19:48:11 -0400

    added helper methods

--- todo_app/todo-mvc/app/helpers/items_helper.rb
+++ todo_app/todo-mvc/app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+    def li_class_for_item(item)
+        "completed" if item.complete?
+    end
+
+    def form_for_item_status(item)
+        form_for([@list, item]) do |f|
+            f.check_box :status, :class => "toggle", :checked => item.complete?
+        end
+    end
 end
